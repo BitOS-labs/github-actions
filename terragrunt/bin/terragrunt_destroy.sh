5706a226f723bcf69736be5431454bfffdd784c3
Yoda <shixi.gs@gmail.com>
2021-11-22 19:54:41 +0800

    fix options

--- terragrunt/bin/terragrunt_destroy.sh
+++ terragrunt/bin/terragrunt_destroy.sh
@@ -3,7 +3,7 @@
 function terragruntDestroy {
   # Gather the output of `terragrunt destroy`.
   echo "destroy: info: destroying Terragrunt-managed infrastructure in ${tfWorkingDir}"
-  destroyOutput=$(${tfBinary} destroy -auto-approve -input=false ${*} 2>&1)
+  destroyOutput=$(${tfBinary} destroy --terragrunt-non-interactive -input=false ${*} 2>&1)
   destroyExitCode=${?}
   destroyCommentStatus="Failed"
 
